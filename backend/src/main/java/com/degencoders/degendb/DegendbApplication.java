c3a73ab5abed9694ddc29a35cb35901e7ed9c21c
Tejas Nair <85873779+TejasNair9977@users.noreply.github.com>
2024-01-31 23:14:40 +0530

    initial commit

---
+++ backend/src/main/java/com/degencoders/degendb/DegendbApplication.java
@@ -0,0 +1,13 @@
+package com.degencoders.degendb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegendbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegendbApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/degencoders/degendb/DegendbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.degencoders.degendb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegendbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

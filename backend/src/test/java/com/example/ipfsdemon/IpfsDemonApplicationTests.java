c3bd6ac7eaac371208fff9997c254ac9b4fa3e9f
Tejas Nair <85873779+TejasNair9977@users.noreply.github.com>
2024-04-15 09:49:44 +0530

    added backend, added frontend template

--- backend/src/test/java/com/example/ipfsdemon/IpfsDemonApplicationTests.java
+++ backend/src/test/java/com/example/ipfsdemon/IpfsDemonApplicationTests.java
@@ -1,13 +1,13 @@
-package com.degencoders.degendb;
+package com.example.ipfsdemon;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DegendbApplicationTests {
+class IpfsDemonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

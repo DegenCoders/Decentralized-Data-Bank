50eac2d59108e466d00a70bd92a33326f631adcf
gurmatsinghsour <89722217+gurmatsinghsour@users.noreply.github.com>
2024-04-17 16:06:53 +0530

    mobile responsive navbar

--- frontend/decentralized-data-bank/app/Navbar.js
+++ frontend/decentralized-data-bank/app/Navbar.js
@@ -1,6 +1,14 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 
 const Navbar = () => {
+
+    const [isClick, setisClick] = useState();
+    const toggleNavbar = () => {
+        setisClick(!isClick);
+    }
+
   return (
     <nav className='bg-black'>
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -32,8 +40,79 @@ const Navbar = () => {
                         </a>
                     </div>
                 </div>
+                <div className='md:hidden flex items-center'>
+                    <button
+                    className='inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:text-white 
+                    focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+                    onClick={toggleNavbar}
+                    >
+                    {isClick ? (
+                        <svg
+                        className='h-6 w-6'
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox="0 0 24 24"
+                        stroke='currentColor'>
+                          
+                        
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="m6 18L18 6M6 6l12 12"
+                        />
+                        </svg>
+                    ) : (
+
+                        <svg
+                        className='h-6 w-6'
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox="0 0 24 24"
+                        stroke='currentColor'
+                        >
+                        <path
+                        strokeLinecap='round'
+                        strokeLinejoin='round'
+                        strokeWidth = {2}
+                        d="M4 6h16M4 12h16m-7 6h7"/>
+
+                        
+                        </svg>
+
+                    )
+                }
+
+                    </button>
+                </div>
             </div>
         </div>
+        {isClick && (
+            <div className='md:hidden'>
+                <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
+                <a href='/' className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                            Upgrade to #number# 
+                            
+                        </a>
+                        <a href='/' className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                            English 
+                            
+                        </a>
+                        <a href='/' className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                            Help 
+
+                        </a>
+                        <a href='/' className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                            Actions 
+                        </a>
+                        <a href='/' className="text-white hover:bg-white hover:text-black rounded-lg p-2">
+                            Login
+                        </a>
+                </div>
+            </div>
+        )}
+
+
     </nav>
   )
 }

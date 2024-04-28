f2f1f2464e100840c3c5439d2bc9388226641701
Tejas Nair <85873779+TejasNair9977@users.noreply.github.com>
2024-04-28 17:16:28 +0530

    addded code for seeing listener progress

--- backend/src/utils/torrent.js
+++ backend/src/utils/torrent.js
@@ -7,38 +7,35 @@ const clientOpts = {
 }
 
 export function createTorrentfromFile(filePath) {
-    client.seed("./target", clientOpts, (torrent) => {
-        console.log('Magnet Torrent:', torrent.magnetURI);
-        console.log('Added torrent:', torrent.name);
-        console.log("Torrent progress:", torrent.progress)
-        console.log('Torrent Path:', torrent.path); 
-        console.log("Torrent Readiness:", torrent.ready)
-    });
-
+    const torrent = client.seed("./target", clientOpts)
+    console.log('Magnet Torrent:', torrent.magnetURI);
+    console.log('Added torrent:', torrent.name);
+    console.log("Torrent progress:", torrent.progress)
+    console.log('Torrent Path:', torrent.path); 
+    console.log("Torrent Readiness:", torrent.ready)
 }
 
 export function downloadFromTorrent(magnetUri) {
-    client.add(magnetUri, function (torrent) {
-        console.log("Downloading:", torrent.name);
+    console.log(magnetUri)
+    const torrent = client.add(magnetUri, clientOpts)
+    console.log("Downloading:", torrent.name);
 
-        // Listen for download progress
-        torrent.on('download', function (bytes)  {
-            console.log('Just downloaded:', bytes);
-            console.log('Total downloaded:', torrent.downloaded);
-            console.log('Download speed:', torrent.downloadSpeed);
-        });
+    // Listen for download progress
+    torrent.on('download', function (bytes)  {
+        console.log('Just downloaded:', bytes);
+        console.log('Total downloaded:', torrent.downloaded);
+        console.log('Download speed:', torrent.downloadSpeed);
+    });
+    // Listen for completion
+    torrent.on('done', () => {
+        console.log('Download complete');
+        // Do something when download is complete
+    });
 
-        // Listen for completion
-        torrent.on('done', () => {
-            console.log('Download complete');
-            // Do something when download is complete
-        });
-
-        // Listen for errors
-        torrent.on('error', (err) => {
-            console.log('Error downloading:', err);
-            // Handle error
-        });
+    // Listen for errors
+    torrent.on('error', (err) => {
+        console.log('Error downloading:', err);
+        // Handle error
     });
 }
  
